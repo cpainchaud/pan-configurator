bebb9017efe178eb1ec17b6ae38a57c3e988f598
Christophe Painchaud <shellescape@gmail.com>
2014-12-10 18:19:53 +0100

    more licenses fixes

--- lib/class-CsvParser.php
+++ lib/class-CsvParser.php
@@ -1,10 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cpainchaud
- * Date: 17/11/2014
- * Time: 07:18
- */
+
+/*
+ * Copyright (c) 2014 Palo Alto Networks, Inc. <info@paloaltonetworks.com>
+ * Author: Christophe Painchaud cpainchaud _AT_ paloaltonetworks.com
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
 
 class CsvParser
 {
