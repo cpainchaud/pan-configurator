b3807878dc01c8887096d1ec06b6c2d10ce1ddc2
Christophe Painchaud <shellescape@gmail.com>
2015-09-16 14:18:31 +0200

    fix division by 0

--- lib/panconfigurator.php
+++ lib/panconfigurator.php
@@ -487,6 +487,8 @@ class XmlArray {
 
 function convert($size)
 {
+    if( $size == 0 )
+        return '0';
     $unit=array('b','kb','mb','gb','tb','pb');
     return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
 }
