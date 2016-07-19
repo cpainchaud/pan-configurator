59b8175e18e9354e173a063823074cfb6b10b00a
Christophe Painchaud <cpainchaud@paloaltonetworks.com>
2016-07-19 12:39:55 +0200

    updated numbering to 1.5.11

--- lib/misc-classes/class-PH.php
+++ lib/misc-classes/class-PH.php
@@ -37,7 +37,7 @@ class PH
 
     private static $library_version_major = 1;
     private static $library_version_sub = 5;
-    private static $library_version_bugfix = 10;
+    private static $library_version_bugfix = 11;
 
     static public function frameworkVersion()
     {
