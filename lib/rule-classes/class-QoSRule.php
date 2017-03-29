f9ba61064d799b7cff1852e6c29fc729490dbdc2
Christophe Painchaud <cpainchaud@paloaltonetworks.com>
2017-03-29 16:08:27 +0200

    framework doc update

--- lib/rule-classes/class-QoSRule.php
+++ lib/rule-classes/class-QoSRule.php
@@ -1,7 +1,6 @@
 <?php
 /*
- * Copyright (c) 2014-2015 Palo Alto Networks, Inc. <info@paloaltonetworks.com>
- * Author: Christophe Painchaud <cpainchaud _AT_ paloaltonetworks.com>
+ * Copyright (c) 2014-2017 Christophe Painchaud <shellescape _AT_ gmail.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
