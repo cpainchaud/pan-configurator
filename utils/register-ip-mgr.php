29d9e075fc152e36c79466cb8722b80d38be78df
Sven Waschkut <swaschkut@paloaltonetworks.com>
2019-01-07 16:31:59 +0100

    register-ip-mgr | introduce new util to display and unregister-unused dynamic registered-ip addresses

--- utils/register-ip-mgr.php
+++ utils/register-ip-mgr.php
@@ -10,7 +10,7 @@ require_once(dirname(__FILE__).'/common/misc.php');
 $debugAPI = false;
 
 $supportedArguments = Array();
-$supportedArguments[] = Array('niceName' => 'Action', 'shortHelp' => 'type of action you want to perform against API', 'argDesc' => 'display');
+$supportedArguments[] = Array('niceName' => 'Actions', 'shortHelp' => 'type of action you want to perform against API', 'argDesc' => 'display');
 $supportedArguments[] = Array('niceName' => 'in', 'shortHelp' => 'the target PANOS device ie: in=api://1.2.3.4', 'argDesc' => 'api://[hostname or IP]');
 $supportedArguments[] = Array('niceName' => 'Location', 'shortHelp' => 'defines the VSYS target of the UserID request', 'argDesc' => 'vsys1[,vsys2,...]');
 $supportedArguments[] = Array('niceName' => 'help', 'shortHelp' => 'this message');
@@ -116,7 +116,7 @@ echo "\n";
 
 
 
-if( !isset(PH::$args['action']) )
+if( !isset(PH::$args['actions']) )
     display_error_usage_exit("no 'action' was defined");
 
 $location = 'vsys1';
@@ -130,21 +130,27 @@ else
 
 
 
-$action = strtolower(PH::$args['action']);
+$action = strtolower(PH::$args['actions']);
 
-if( $action == 'display' )
+if( $action == 'display' || $action == 'unregister-unused')
 {
     echo " - action is '$action'\n";
     print "\n\n\n";
 
+    $unregister_array = array();
 
     $pan->load_from_domxml($xmlDoc);
-    foreach( $pan->getVirtualSystems() as $sub )
+    $virtualsystems = $pan->getVirtualSystems();
+    foreach( $virtualsystems as $sub )
     {
-        echo "\n - ".$sub->name()."\n";
+        $unregister_array[$sub->name()] = array();
 
-        print "     - IPs: \n";
+        print "\n\n##################################\n";
+        echo PH::boldText( " - ".$sub->name()."\n" );
+
         $register_ip_array = $connector->register_getIp( $sub->name() );
+        print "     - registered-ips: [".count($register_ip_array)."]\n";
+
         foreach ( $register_ip_array  as $ip => $reg )
         {
             $first_value = reset($reg); // First Element's Value
@@ -156,7 +162,21 @@ if( $action == 'display' )
 
         $vsys = $pan->findVirtualSystem( $sub->name() );
         $address_groups = $vsys->addressStore->addressGroups();
+
+        $shared_address_groups = $pan->addressStore->addressGroups();
+
+        $address_groups = array_merge( $shared_address_groups, $address_groups );
         print "     - DAGs: \n";
+        /*
+        foreach( $shared_address_groups as $addressGroup)
+        {
+            if( $addressGroup->isDynamic() )
+            {
+                print "          ".$addressGroup->name()." filter: ".$addressGroup->filter."\n";
+            }
+        }
+        */
+        $dynamicAddressGroup_array = array();
         foreach( $address_groups as $addressGroup)
         {
             if( $addressGroup->isDynamic() )
@@ -165,19 +185,111 @@ if( $action == 'display' )
 
                 print "          ".$addressGroup->name()." filter: ".$addressGroup->filter."\n";
 
+                $dynamicAddressGroup_array = $connector->dynamicAddressGroup_get( $sub->name() );
+                if( isset( $dynamicAddressGroup_array[$addressGroup->name()] ) )
+                    foreach( $dynamicAddressGroup_array[$addressGroup->name()] as $key => $members )
+                    {
+                        if( $key != 'name' )
+                        {
+                            print "           - ".$key."\n";
+                        }
+
+                    }
+
             }
         }
 
-        $dynamicAddressGroup_array = $connector->dynamicAddressGroup_get( $sub->name() );
-        print "REGISTERED-IP:\n";
-        print_r( $register_ip_array );
-        print "DAG with registered-ip:\n";
-        print_r( $dynamicAddressGroup_array );
 
+        print "\n\n----------------------------------\n";
+        print "VALIDATION:\n\n";
+
+        if( empty( $register_ip_array ) )
+        {
+            #print "nothing registered\n";
+        }
+        else
+        {
+            #print "which registered-ip can be deleted because:\n";
+            #print "  - no DAG for tag is available\n";
+            #print "  - DAG is not used, so no registered-ip for DAG\n";
+
+
+            foreach ( $register_ip_array  as $ip => $reg )
+            {
+                $first_value = reset($reg); // First Element's Value
+                $first_key = key($reg); // First Element's Key
+
+                foreach( $dynamicAddressGroup_array as $key => $group )
+                {
+                    #print "KEY: ".$key."\n";
+                    #print_r( $group );
+                    if( !isset( $group[$ip] )  )
+                    {
+                        $unregister_array[$sub->name()][$ip] = $reg;
+                        #print "unregister: ".$ip."\n";
+                    }
+                    else
+                    {
+                        #print "unset: ".$ip."\n";
+                        unset( $unregister_array[$sub->name()][$ip] );
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        print "possible IPs for UNREGISTER:\n";
+        #print_r( $unregister_array );
+        foreach( $unregister_array[$sub->name()] as $unregister_ip => $tags )
+        {
+            print " - ".$unregister_ip."\n";
+        }
+
+        print "DAGs can be deleted (because they are not used in Ruleset):\n";
+        foreach( $dynamicAddressGroup_array as $key => $group )
+        {
+            if( count( $group ) <= 1 )
+                print " - ".$key . "\n";
+        }
     }
+}
+elseif( $action == 'fakeregister' )
+{
+    $numberOfIPs = 499;
+    $tag = 'fake';
+    $startingIP = ip2long('10.0.0.0');
+
+    $records = Array();
+
+
+    echo "  - Generating {$numberOfIPs} fake records starting at IP ".long2ip($startingIP)."... ";
+    for($i=1; $i<= $numberOfIPs; $i++)
+    {
+        $records[long2ip($startingIP+$i)] = array( $tag );
+    }
+    echo "OK!\n";
+
+
+    echo " - now sending records to API ... ";
+    $connector->register_sendUpdate( $records, null, 'vsys1' );
+    echo "OK!\n";
+
 }
 else
     derr("action '{$action}' is not supported");
+
+
+if(  $action == 'unregister-unused')
+{
+    foreach( $virtualsystems as $sub )
+    {
+        echo " - now sending records to API ... ";
+        $connector->register_sendUpdate(null, $unregister_array[$sub->name()], $sub->name());
+        echo "OK!\n";
+    }
+}
+
 
 
 
