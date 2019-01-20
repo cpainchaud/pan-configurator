8b10ffcb1a3fea476312901fe9bb4ccfa7e06c80
Sven Waschkut <swaschkut@paloaltonetworks.com>
2019-01-20 14:55:24 +0100

    address-edit: new actions=name-rename-character:[search],[replace]

--- utils/common/actions-address.php
+++ utils/common/actions-address.php
@@ -1104,6 +1104,60 @@ AddressCallContext::$supportedActions[] = Array(
 );
 
 AddressCallContext::$supportedActions[] = Array(
+    'name' => 'name-Rename-Character',
+    'MainFunction' =>  function ( AddressCallContext $context )
+    {
+        $object = $context->object;
+
+        if( $object->isTmpAddr() )
+        {
+            echo $context->padding." *** SKIPPED : not applicable to TMP objects\n";
+            return;
+        }
+
+        $characterToreplace = $context->arguments['oldCharacter'];
+        $characterForreplace = $context->arguments['newCharacter'];
+
+
+        $newName = str_replace( $characterToreplace, $characterForreplace, $object->name() );
+
+
+        if( $object->name() == $newName )
+        {
+            echo $context->padding." *** SKIPPED : new name and old name are the same\n";
+            return;
+        }
+
+        echo $context->padding." - new name will be '{$newName}'\n";
+
+        $findObject = $object->owner->find($newName);
+        if( $findObject !== null )
+        {
+            echo $context->padding." *** SKIPPED : an object with same name already exists\n";
+            return;
+        }
+        else
+        {
+            echo $context->padding." - renaming object... ";
+            if( $context->isAPI )
+                $object->API_setName($newName);
+            else
+                $object->setName($newName);
+            echo "OK!\n";
+        }
+
+    },
+    'args' => Array( 'oldCharacter' => Array(
+        'type' => 'string',
+        'default' => '*nodefault*'),
+        'newCharacter' => Array(
+            'type' => 'string',
+            'default' => '*nodefault*')
+    ),
+    'help' => ''
+);
+
+AddressCallContext::$supportedActions[] = Array(
     'name' => 'name-addPrefix',
     'MainFunction' =>  function ( AddressCallContext $context )
     {
