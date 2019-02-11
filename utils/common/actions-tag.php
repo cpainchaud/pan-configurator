65bdf853c17925742a7dd378703306136590dcdc
Sven Waschkut <swaschkut@paloaltonetworks.com>
2019-02-12 00:26:15 +0100

    tag-edit: new actions=exporttoexcel:[filename]

--- utils/common/actions-tag.php
+++ utils/common/actions-tag.php
@@ -494,3 +494,155 @@ TagCallContext::$supportedActions[] = Array(
         'mode' => Array( 'type' => 'string', 'default' => 'skipIfConflict', 'choices' => Array( 'skipIfConflict', 'removeIfMatch') )
     ),
 );
+
+TagCallContext::$supportedActions[] = Array(
+    'name' => 'exportToExcel',
+    'MainFunction' => function(TagCallContext $context)
+    {
+        $object = $context->object;
+        $context->objectList[] = $object;
+    },
+    'GlobalInitFunction' => function(TagCallContext $context)
+    {
+        $context->objectList = Array();
+    },
+    'GlobalFinishFunction' => function(TagCallContext $context)
+    {
+        $args = &$context->arguments;
+        $filename = $args['filename'];
+
+        $lines = '';
+        $encloseFunction  = function($value, $nowrap = true)
+        {
+            if( is_string($value) )
+                $output = htmlspecialchars($value);
+            elseif( is_array($value) )
+            {
+                $output = '';
+                $first = true;
+                foreach( $value as $subValue )
+                {
+                    if( !$first )
+                    {
+                        $output .= '<br />';
+                    }
+                    else
+                        $first= false;
+
+                    if( is_string($subValue) )
+                        $output .= htmlspecialchars($subValue);
+                    else
+                        $output .= htmlspecialchars($subValue->name());
+                }
+            }
+            else
+                derr('unsupported');
+
+            if( $nowrap )
+                return '<td style="white-space: nowrap">'.$output.'</td>';
+
+            return '<td>'.$output.'</td>';
+        };
+
+
+        $addWhereUsed = false;
+        $addUsedInLocation = false;
+
+        $optionalFields = &$context->arguments['additionalFields'];
+
+        if( isset($optionalFields['WhereUsed']) )
+            $addWhereUsed = true;
+
+        if( isset($optionalFields['UsedInLocation']) )
+            $addUsedInLocation = true;
+
+
+        $headers = '<th>location</th><th>name</th><th>color</th><th>description</th>';
+
+        if( $addWhereUsed )
+            $headers .= '<th>where used</th>';
+        if( $addUsedInLocation )
+            $headers .= '<th>location used</th>';
+
+        $count = 0;
+        if( isset($context->objectList) )
+        {
+            foreach ($context->objectList as $object)
+            {
+                $count++;
+
+                /** @var Tag $object */
+                if ($count % 2 == 1)
+                    $lines .= "<tr>\n";
+                else
+                    $lines .= "<tr bgcolor=\"#DDDDDD\">";
+
+                $lines .= $encloseFunction(PH::getLocationString($object));
+
+                $lines .= $encloseFunction($object->name());
+
+                if ( $object->isTag() )
+                {
+                    if( $object->isTmp() )
+                    {
+                        $lines .= $encloseFunction('unknown');
+                        $lines .= $encloseFunction('');
+
+                    }
+                    else
+                    {
+                        $lines .= $encloseFunction($object->color);
+                        $lines .= $encloseFunction($object->getComments());
+                    }
+                }
+
+                if( $addWhereUsed )
+                {
+                    $refTextArray = Array();
+                    foreach( $object->getReferences() as $ref )
+                        $refTextArray[] = $ref->_PANC_shortName();
+
+                    $lines .= $encloseFunction($refTextArray);
+                }
+                if( $addUsedInLocation )
+                {
+                    $refTextArray = Array();
+                    foreach( $object->getReferences() as $ref )
+                    {
+                        $location = PH::getLocationString($object->owner);
+                        $refTextArray[$location] = $location;
+                    }
+
+                    $lines .= $encloseFunction($refTextArray);
+                }
+
+                $lines .= "</tr>\n";
+            }
+        }
+
+        $content = file_get_contents(dirname(__FILE__).'/html-export-template.html');
+        $content = str_replace('%TableHeaders%', $headers, $content);
+
+        $content = str_replace('%lines%', $lines, $content);
+
+        $jscontent =  file_get_contents(dirname(__FILE__).'/jquery-1.11.js');
+        $jscontent .= "\n";
+        $jscontent .= file_get_contents(dirname(__FILE__).'/jquery.stickytableheaders.min.js');
+        $jscontent .= "\n\$('table').stickyTableHeaders();\n";
+
+        $content = str_replace('%JSCONTENT%', $jscontent, $content);
+
+        file_put_contents($filename, $content);
+    },
+    'args' => Array(    'filename' => Array( 'type' => 'string', 'default' => '*nodefault*' ),
+        'additionalFields' =>
+            Array( 'type' => 'pipeSeparatedList',
+                'subtype' => 'string',
+                'default' => '*NONE*',
+                'choices' => Array('WhereUsed', 'UsedInLocation'),
+                'help' =>
+                    "pipe(|) separated list of additional field to include in the report. The following is available:\n".
+                    "  - WhereUsed : list places where object is used (rules, groups ...)\n".
+                    "  - UsedInLocation : list locations (vsys,dg,shared) where object is used\n")
+    )
+);
